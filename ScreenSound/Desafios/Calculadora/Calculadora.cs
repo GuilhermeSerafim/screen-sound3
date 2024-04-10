c81ad39b29045eb55a74b3e708ced3daecef338c
GuilhermeSerafim <guilerstudies@gmail.com>
2024-04-10 16:38:51 -0300

    Criando calculadora simples - desafio

---
+++ ScreenSound/Desafios/Calculadora/Calculadora.cs
@@ -0,0 +1,31 @@
+﻿namespace ScreenSound.Desafios.Calculadora
+{
+    internal class Calculadora
+    {
+
+        public void Soma(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public void Subtracao(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+        public void Multiplicacao(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+        public void Divisao(int a, int b)
+        {
+            Console.WriteLine(a / b);
+        }
+        public void Potencia(int a, int b)
+        {
+            Console.WriteLine(a ^ b);
+        }
+        public void RaizQuadrada(int a)
+        {
+            Console.WriteLine(Math.Sqrt(a));
+        }
+    }
+}

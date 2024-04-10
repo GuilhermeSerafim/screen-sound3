f00bf594d1f001b30bdcdbbd7eca196fcc0ceddc
GuilhermeSerafim <guilerstudies@gmail.com>
2024-04-10 11:40:22 -0300

    Renomeando função

---
+++ ScreenSound/Desafio/DesafioUso.cs
@@ -0,0 +1,31 @@
+﻿//using Alura.Filmes;
+
+//// Instanciando artistas
+//Artista matthew = new("Matthew", 54);
+//Artista timotthée = new("Timothée", 28);
+//Artista anne = new("Anne", 41);
+//Artista zendaya = new("Zendaya", 27);
+
+//// Ao adicionar o elenco, dinamicamente o elenco aqui muda também
+//Filme interestelar = new("Interestelar", 120);
+//interestelar.AdicionarArtistaAoElenco(matthew);
+//interestelar.AdicionarArtistaAoElenco(timotthée);
+//interestelar.AdicionarArtistaAoElenco(timotthée);
+//interestelar.AdicionarArtistaAoElenco(anne);
+
+//Filme duna2 = new("Duna 2", 150);
+//duna2.AdicionarArtistaAoElenco(timotthée);
+//duna2.AdicionarArtistaAoElenco(zendaya);
+
+////// Desafio parte 1 Concluída
+//interestelar.ListarElenco();
+//Console.WriteLine();
+
+
+
+//// Desafio parte 2 Concluída
+//Filme wonka = new("Wonka", 200);
+//timotthée.AdicionarFilmeAtuado(wonka);
+
+
+//timotthée.MostrarFilmesAtuados();

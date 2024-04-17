7908acdcf17522d2868c704635a057211afa4e5e
GuilhermeSerafim <guilerstudies@gmail.com>
2024-04-17 14:02:32 -0300

    Implementando conceito do IEnumerable para reduzir o acesso somente a leitura

--- ScreenSound/Modelos/Banda.cs
+++ ScreenSound/Modelos/Banda.cs
@@ -3,8 +3,10 @@
 internal class Banda : IAvaliavel // Implementando a interface
 {
     public List<Album> albuns = new();
+    public IEnumerable<Album> Albuns => albuns; // Reduzindo acesso, pois list tem metodos que podem implicar na nossa regra de negocio (Clear por ex)
     private List<Avaliacao> notas = new();
     public string Nome { get; }
+
 
     public Banda(string nome) => Nome = nome;
 
@@ -28,7 +30,7 @@ internal class Banda : IAvaliavel // Implementando a interface
 
     public void ExibirDiscografia()
     {
-        foreach (Album album in albuns)
+        foreach (Album album in Albuns)
         {
             Console.WriteLine($"Álbum: {album.Nome}\n");
             Console.WriteLine($"Duração do álbum em segundos: {album.DuracaoTotal}");
